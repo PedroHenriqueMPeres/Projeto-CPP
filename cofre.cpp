75d925daef9a729b175914aa2f18fa4c764da7a9
PedroHenriqueMPeres <166727356+PedroHenriqueMPeres@users.noreply.github.com>
2024-08-22 11:39:47 -0300

    Add files via upload

---
+++ cofre.cpp
@@ -0,0 +1,43 @@
+// integrantes: Agatha e Pedro.
+#include <iostream>
+using namespace std;
+
+    void menu() {
+   
+    cout << "\n";
+    cout << "   -------------- MENU -------------   \n";
+    cout << " |                                   | \n";
+    cout << " |  - 1) Cadastrar passagem.         | \n";
+    cout << " |  - 2) Listar passagens.           | \n";
+    cout << " |  - 0) Sair.                       | \n";
+    cout << " |                                   | \n";
+    cout << "   ---------------------------------   \n";
+    }
+
+
+int main(){
+    system ("cls");
+    int opcao;
+
+
+    cout << "\n";
+    cout << "                      ---------------------------------   \n";
+    cout << "                    |                                   | \n";
+    cout << "                    |       Bem vindo ao programa       | \n";
+    cout << "                    |                de                 | \n";
+    cout << "                    |         Passagens Aereas          | \n";
+    cout << "                    |                                   | \n";
+    cout << "                      ---------------------------------   \n";
+
+    cout << "o que voce quer ?: ";
+    cin >> menu();
+
+    cout << " " << endl;
+    void desenhaBoasVindas();{
+        cout << " ---> Boas vindas ao menu! :) " << endl;
+
+    }
+
+  
+
+}
